9312f08952044b054e9e36c17e0b0b72f1c072eb
Lucas Zuquetto <lucasdeozuquetto@hotmail.com>
2022-06-13 12:06:33 -0300

    Selecionar elementos e botão de fechar o pedido prontos

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ function selecionarcomida(elemento){
         foodselect.classList.remove('selecionado')
     }
     elemento.classList.add('selecionado')
+    FecharPedido()
 }
 function selecionarbebida(elemento){
     let drinkselect = document.querySelector('.menu-bebida .selecionado')
@@ -11,6 +12,7 @@ function selecionarbebida(elemento){
         drinkselect.classList.remove('selecionado')
     }
     elemento.classList.add('selecionado')
+    FecharPedido()
 }
 function selecionarsobremesa(elemento){
     let dessertselect = document.querySelector('.menu-sobremesa .selecionado')
@@ -18,4 +20,17 @@ function selecionarsobremesa(elemento){
         dessertselect.classList.remove('selecionado')
     }
     elemento.classList.add('selecionado')
+    FecharPedido()
+}
+function FecharPedido(){
+    const ComidaSelecionada = document.querySelector(".menu-prato .selecionado")
+    const BebidaSelecionada = document.querySelector(".menu-bebida .selecionado")
+    const SobremesaSelecionada = document.querySelector(".menu-sobremesa .selecionado")
+    const MudarTexto = document.querySelector(".textopedido")
+    const Botaopedido = document.querySelector(".btn")
+    if (ComidaSelecionada && BebidaSelecionada && SobremesaSelecionada !== null){
+        MudarTexto.innerHTML = "Fechar Pedido"
+        MudarTexto.classList.add('paddingtop')
+        Botaopedido.classList.add('corbotao')
+    }
 }
