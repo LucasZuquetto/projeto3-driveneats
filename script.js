3b77444f09ce4d3e74de6b96a1d7715f45069a27
Lucas Zuquetto <lucasdeozuquetto@hotmail.com>
2022-06-13 14:21:09 -0300

    Requisitos Prontos

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function selecionarcomida(elemento){
-    let foodselect = document.querySelector('.menu-prato .selecionado')
+    const foodselect = document.querySelector('.menu-prato .selecionado')
     if(foodselect !== null){
         foodselect.classList.remove('selecionado')
     }
@@ -7,7 +7,7 @@ function selecionarcomida(elemento){
     FecharPedido()
 }
 function selecionarbebida(elemento){
-    let drinkselect = document.querySelector('.menu-bebida .selecionado')
+    const drinkselect = document.querySelector('.menu-bebida .selecionado')
     if(drinkselect !== null){
         drinkselect.classList.remove('selecionado')
     }
@@ -15,7 +15,7 @@ function selecionarbebida(elemento){
     FecharPedido()
 }
 function selecionarsobremesa(elemento){
-    let dessertselect = document.querySelector('.menu-sobremesa .selecionado')
+    const dessertselect = document.querySelector('.menu-sobremesa .selecionado')
     if(dessertselect !== null){
         dessertselect.classList.remove('selecionado')
     }
@@ -33,4 +33,25 @@ function FecharPedido(){
         MudarTexto.classList.add('paddingtop')
         Botaopedido.classList.add('corbotao')
     }
+}
+function EnviarWpp(){
+    let NomeComida = document.querySelector('.menu-prato .selecionado h3')
+    NomeComida = NomeComida.innerHTML
+    let PrecoComida = document.querySelector('.menu-prato .selecionado span')
+    PrecoComida = PrecoComida.innerHTML
+    let NomeBebida = document.querySelector('.menu-bebida .selecionado h3')
+    NomeBebida = NomeBebida.innerHTML
+    let PrecoBebida = document.querySelector('.menu-bebida .selecionado span')
+    PrecoBebida = PrecoBebida.innerHTML
+    let NomeSobremesa = document.querySelector('.menu-sobremesa .selecionado h3')
+    NomeSobremesa = NomeSobremesa.innerHTML
+    let PrecoSobremesa = document.querySelector('.menu-sobremesa .selecionado span')
+    PrecoSobremesa = PrecoSobremesa.innerHTML
+    PrecoComida = Number(PrecoComida)
+    PrecoBebida = Number(PrecoBebida)
+    PrecoSobremesa = Number(PrecoSobremesa)
+    let Precototal = Number(PrecoComida + PrecoBebida + PrecoSobremesa).toFixed(2)
+    const Mensagem = `Olá, Gostaria de fazer o pedido:\n- Prato: ${NomeComida}\n- Bebida: ${NomeBebida}\n- Sobremesa: ${NomeSobremesa}\nTotal: R$ ${Precototal}`
+    const EnviarMensagem = `https://wa.me/5555999738820?text=${encodeURIComponent(Mensagem)}`
+    window.open(EnviarMensagem)
 }
